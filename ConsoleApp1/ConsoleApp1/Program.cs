130495fd8dcd3bde2c675ad83bedb284826815b3
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2019-03-20 13:28:09 +0100

    -ucitavanje brojeva se mora raditi prkeo stringova -njihove konverzije u primitivne tipove preko omotaca kao sto su Int32, Int16, Int64, pri cemu oni oznacavaju 'signed' ili predznacne brojeve, a UInt32 predstavlja nepredznacni broj ili 'unsigned'

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,15 @@ namespace ConsoleApp1
 {
     class Program
     {
+        const int CVOR_METARA = 1852;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Console.WriteLine("Unesite brzinu broda u čvorovima: ");
+            int brzinaUCvorovima = Convert.ToInt32(Console.ReadLine());
+            //1cvor = 1 morskaMilja/h
+            Console.WriteLine("brod ide {0:F2} km/h", (brzinaUCvorovima*(CVOR_METARA/1000.0)));
+            Console.ReadLine();
         }
     }
 }
